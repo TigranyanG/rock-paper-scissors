d7b04451e7055b1268fa6cd8f1be2e9293260ac2
TigranyanG <grisha.tigranyan.2006@gmail.com>
2025-10-27 16:59:45 -0500

    feat: Convert random num to rock/paper/scissors
    Converted the random to words.
    
    1 => rock
    2 => paper
    3 => scissors

--- javascript.js
+++ javascript.js
@@ -1,4 +1,6 @@
 function getComputerChoice(){
     let a = Math.floor(Math.random() * 3) + 1;
-    return a;
+    if(a === 1) return "rock";
+    else if (a === 2) return "paper";
+    else if (a === 3) return "scissors";
 }
