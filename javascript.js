4789ec5b7cff3388226add07f6e611d5f25bc44c
TigranyanG <grisha.tigranyan.2006@gmail.com>
2025-10-27 18:49:12 -0500

    feat: add function playGame, 5 rnd, declare winner

--- javascript.js
+++ javascript.js
@@ -13,47 +13,70 @@ function getHumanChoice(){
 let humanScore = 0;
 let computerScore = 0;
 
-function playRound(computerChoice, humanChoice){
-    computerChoice = getComputerChoice();
-    humanChoice = getHumanChoice().toLowerCase();
 
-    if(computerChoice === "rock"){
-        if(humanChoice === "rock"){
-            console.log(`Noone wins - Both entered rock :(`);
+
+function playGame(){
+    function playRound(computerChoice, humanChoice){
+
+        computerChoice = getComputerChoice();
+        humanChoice = getHumanChoice().toLowerCase();
+
+        if(computerChoice === "rock"){
+            if(humanChoice === "rock"){
+                console.log(`Noone wins - Both entered rock :(`);
+            }
+            else if(humanChoice !== "paper"){
+                console.log(`You lose! Rock beats ${humanChoice}`);
+                computerScore++;
+            }
+            else{
+                console.log(`You win! ${humanChoice.charAt(0).toUpperCase() + humanChoice.substring(1)} beats rock`);
+                humanScore++;
+            }
         }
-        else if(humanChoice !== "paper"){
-            console.log(`You lose! Rock beats ${humanChoice}`);
-            computerScore++;
+        else if(computerChoice === "paper"){
+            if(humanChoice === "paper"){
+                console.log(`Noone wins - Both entered paper :(`);
+            }
+            else if(humanChoice !== "scissors"){
+                console.log(`You lose! Paper beats ${humanChoice}`);
+                computerScore++;
+            }
+            else{
+                console.log(`You win! ${humanChoice.charAt(0).toUpperCase() + humanChoice.substring(1)} beats paper`);
+                humanScore++;
+            }
         }
-        else{
-            console.log(`You win! ${humanChoice.charAt(0).toUpperCase() + humanChoice.substring(1)} beats rock`);
-            humanScore++;
+        else if(computerChoice === "scissors"){
+            if(humanChoice === "scissors"){
+                console.log(`Noone wins - Both entered scissors :(`);
+            }
+            else if(humanChoice !== "rock"){
+                console.log(`You lose! Scissors beat ${humanChoice}`);
+                computerScore++;
+            }
+            else{
+                console.log(`You win! ${humanChoice.charAt(0).toUpperCase() + humanChoice.substring(1)} beats scissors`);
+                humanScore++;
+            }
         }
+
+    }  
+
+    for(let i = 0; i < 5; i++){
+        playRound();
+        console.log(humanScore);
+        console.log(computerScore);
     }
-    else if(computerChoice === "paper"){
-        if(humanChoice === "paper"){
-            console.log(`Noone wins - Both entered paper :(`);
-        }
-        else if(humanChoice !== "scissors"){
-            console.log(`You lose! Paper beats ${humanChoice}`);
-            computerScore++;
-        }
-        else{
-            console.log(`You win! ${humanChoice.charAt(0).toUpperCase() + humanChoice.substring(1)} beats paper`);
-            humanScore++;
-        }
+    if (humanScore > computerScore){
+        console.log("Congratulations, you won the game!");
     }
-    else if(computerChoice === "scissors"){
-        if(humanChoice === "scissors"){
-            console.log(`Noone wins - Both entered scissors :(`);
-        }
-        else if(humanChoice !== "rock"){
-            console.log(`You lose! Scissors beat ${humanChoice}`);
-            computerScore++;
-        }
-        else{
-            console.log(`You win! ${humanChoice.charAt(0).toUpperCase() + humanChoice.substring(1)} beats scissors`);
-            humanScore++;
-        }
+    else if(computerScore > humanScore){
+        console.log("Pity, you lost the game!");
+    }
+    else{
+        console.log("Friendship wins! It's a tie.");
     }
 }
+
+playGame();
